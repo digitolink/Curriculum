4f7064ea34e166e8ceddef7e7858c3f95787618b
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-11 11:31:07 +0100

    nuevo commit

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function main(){
     console.log("Starting...");
+    const titulo = document.querySelector("h1");
+    titulo.addEventListener("pointerenter",imprimeHola);
 }
+function imprimeHola(){
+    console.log("Hola2");
+}
+
 window.addEventListener("load", main);
