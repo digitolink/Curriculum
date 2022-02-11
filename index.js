38b15e76a9ff807c7b8c160ac7c0b3c9c6cb5a8d
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-11 12:37:34 +0100

    nuevo commit

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ function main(){
     console.log("Starting...");
     const titulo = document.querySelector("h1");
     titulo.addEventListener("pointerenter",imprimeHola);
+    const elements =  Array.from(document.body.querySelectorAll("*"));
+    for (let items of elements) {
+        //console.log(items);
+        items.addEventListener("pointerenter", ()=>{console.log("POP")}
+        )
+    
+    };
 }
 function imprimeHola(){
     console.log("Hola2");
